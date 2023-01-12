430f07dca92d24db2dd6f021a775bb02c044ed44
SamS2007 <118993453+SamS2007@users.noreply.github.com>
2023-01-12 11:04:57 -0500

    moves inchworm

--- main.ts
+++ main.ts
@@ -1,15 +1,36 @@
-let go = 0
 input.onButtonPressed(Button.A, function () {
     go = 1
+})
+input.onButtonPressed(Button.AB, function () {
+    go = 2
 })
 input.onButtonPressed(Button.B, function () {
     go = 0
 })
+let go = 0
+basic.showLeds(`
+    . # # # .
+    . # # # .
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+music.playMelody("G F G A - F E D ", 120)
+music.playMelody("C D E F G A B C5 ", 120)
+music.playMelody("C D E F G A B C5 ", 120)
 basic.forever(function () {
     if (go == 1) {
         servos.P0.setAngle(0)
         basic.pause(500)
         servos.P0.setAngle(180)
+        basic.pause(500)
+    }
+})
+basic.forever(function () {
+    if (go == 2) {
+        servos.P0.setAngle(0)
+        basic.pause(500)
+        servos.P0.setAngle(90)
         basic.pause(500)
     }
 })
